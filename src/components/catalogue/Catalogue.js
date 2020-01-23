d6c48a9593bd6b60abc3845c21105bd2aedc50d9
Rodrigo Sebastián Zenteno Martínez <rozenteno@Rodrigos-MacBook-Pro.local>
2020-01-23 14:14:04 -0600

    Testing push

--- src/components/catalogue/Catalogue.js
+++ src/components/catalogue/Catalogue.js
@@ -4,7 +4,7 @@ import Sidebar from "../sidebar/Sidebar";
 import ProductItem from "../product/ProductItem";
 
 const Catalogue = () => {
-  return <h1>Catalogue</h1>;
+  return <h1>Catalogue de quentia</h1>;
 };
 
 export default Catalogue;
